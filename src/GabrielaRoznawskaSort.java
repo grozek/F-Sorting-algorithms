9cd4c2a67b05e4fa6be058e9b49a5428ab2361c2
grozek <roznawsk@grinnell.edu>
2023-11-05 00:51:49 -0500

    Functional new sorting algorithm. Passes tests

--- src/GabrielaRoznawskaSort.java
+++ src/GabrielaRoznawskaSort.java
@@ -1,6 +1,6 @@
+
 import java.util.Comparator;
-import java.util.ArrayList;
-import java.util.Arrays;
+
 
 /**
  * Sort using merge sort and insertion sort mix
@@ -20,7 +20,7 @@ public class GabrielaRoznawskaSort implements Sorter {
   /**
    * The one sorter you can access.
    */
-  public static Sorter SORTER = new MergeSort();
+  public static Sorter SORTER = new GabrielaRoznawskaSort();
 
   // +--------------+------------------------------------------------
   // | Constructors |
@@ -34,6 +34,72 @@ public class GabrielaRoznawskaSort implements Sorter {
   // +---------+-----------------------------------------------------
   // | Methods |
   // +---------+
+  public <T> void swap(T[] values, int a, int b) {
+    T temp = values[a];
+    values[a] = values[b];
+    values[b] = temp;
+  }// swap
 
-  public <T> void SortHelper(T[] values, Comparator<? super T> order, int left, int right) {}
+
+  @Override
+
+  public <T> void sort (T[] values, Comparator <? super T> order){
+    int left = 1;
+    int right = values.length-2;
+    int i = 0;
+    while (i < values.length/2){
+      sortHelper(values, order, left, right);
+      i++;
+    }
+  }
+
+  public <T> void sortHelper (T[] values, Comparator <? super T> order, int left, int right){
+    while  (right > 0 && left < values.length-1){
+      if(order.compare(values[left-1], values[left]) > 0){
+        swap(values, left, left-1);
+    }
+      if(order.compare(values[left], values[left+1]) > 0){
+        swap(values, left, left+1);
+    }
+      if(order.compare(values[right], values[right-1]) < 0){
+        swap(values, right, right-1);
+    }
+      if(order.compare(values[right+1], values[right]) < 0){
+        swap(values, right, right+1);
+    }
+    right--;
+    left++;
+    
+  } 
 }
+}
+  // @Override
+  // public <T> void sort(T[] values, Comparator<? super T> order) {
+  //   int j = 1;
+  //   int n = 0;
+  //   sortHelper(values, order, j, n);
+  //   j = 2;
+  //   while(j < values.length-1){
+  //   sortHelper(values, order, j, n);
+  //   j = j+2;
+  //   }
+  // }
+
+
+  // public <T> void sort(T[] values, Comparator<? super T> order) {
+
+  // }
+
+//   public <T> void sortHelper(T[]values, Comparator<? super T> order, int j, int n){
+//     while (n < values.length){
+//       if(order.compare(values[n], values[n+j]) < 0){
+        
+//       }
+//       if(order.compare(values[n], values[n+j]) > 0){
+//         swap(values, n, n+j);
+        
+//       }
+//       n = n+2;
+//     }    
+//   }
+// }
---
+++ src/GabrielaRoznawskaSortTester.java
@@ -0,0 +1,16 @@
+
+
+public class GabrielaRoznawskaSortTester extends SortTester{
+  
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  public GabrielaRoznawskaSortTester() {
+    super();
+    this.sorter = GabrielaRoznawskaSort.SORTER;
+  } // QuicksortTester()
+
+} // class GabrielaRoznawskaSortTester
+
+
